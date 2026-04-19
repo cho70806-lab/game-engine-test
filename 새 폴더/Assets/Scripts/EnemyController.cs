8f1fc7ad2c8c2a5e2474b17e287dd640b01bf559
cho70806 <cho70806@gmail.com>
2026-04-20 02:33:26 +0900

    [feat]스테이지2까지 완성

--- 새 폴더/Assets/Scripts/EnemyController.cs
+++ 새 폴더/Assets/Scripts/EnemyController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    
     public float moveSpeed = 3f;
 
     private Rigidbody2D rb;
@@ -12,9 +13,11 @@ public class EnemyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+  
     // Update is called once per frame
     void Update()
     {
+
         if (isMovingRight)
             rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
         else
